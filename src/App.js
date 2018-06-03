bf01e1fbeb47c2cd2dfc4bbb0d657e00bfde7b53
Luke Zautke <me@lukezautke.com>
2018-06-03 12:26:12 -0600

    Comment out Apollo code because lighterweight

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React, { Component } from "react";
-import ApolloClient, { createNetworkInterface } from "apollo-client";
-import { ApolloProvider } from "react-apollo";
-
-import GitHubConnector from "./components/GitHubConnector";
+// import ApolloClient, { createNetworkInterface } from "apollo-client";
+// import { ApolloProvider } from "react-apollo";
 
 import logo from "./logo.svg";
 import "./App.sass";
 
-const networkInterface = createNetworkInterface({
-  uri: "https://api.github.com/graphql",
-  opts: {
-    headers: {
-      Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`
-    }
-  }
-});
+// const networkInterface = createNetworkInterface({
+//   uri: "https://api.github.com/graphql",
+//   opts: {
+//     headers: {
+//       Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`
+//     }
+//   }
+// });
 
-const client = new ApolloClient({
-  networkInterface
-});
+// const client = new ApolloClient({
+//   networkInterface
+// });
 
 class App extends Component {
   render() {
     return (
-      <ApolloProvider client={client}>
-        <div className="App">
-          <GitHubConnector />
+      // <ApolloProvider client={client}>
+        <div className="app">
+          <p>Yo.</p>
         </div>
-      </ApolloProvider>
+      // </ApolloProvider>
     );
   }
 }
